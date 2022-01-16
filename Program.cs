7e47e09c7d6de37f73db9688bb36812c91561271
krystschr <krystian.schroeder18@gmail.com>
2022-01-16 13:57:46 +0100

    comments

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, Majstry!");
+﻿using System;
+// klasy:
+// pracownik => mechanik, księgowa;
+// samochód,
+// część,
+// narzędzia,
+// zlecenie
+
+
+namespace Warstat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        
+        
+        }
+    }
+}
