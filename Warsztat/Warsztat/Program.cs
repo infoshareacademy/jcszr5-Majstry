609c541937f5f4c435284466f33c6027b19edf7e
Artur Józwiak <artur_jozwiak@o2.pl>
2022-01-22 20:14:33 +0100

    Class Order added.

---
+++ Warsztat/Warsztat/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using static Warsztat.Order;
+
+namespace Warsztat
+{
+
+    class Program
+    {
+       static void Main(string[] args)
+        {
+            List<Order> orders = new List<Order>();
+            Order order = new Order();
+            order.CreateNewOrder(order);
+            orders.Add(order);
+            order.PrintAllOrders(orders);
+        }
+
+    }
+}
