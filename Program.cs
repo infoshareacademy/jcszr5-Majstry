a55db925323e3d52d736b88c7cc5926988842243
krystschr <95431047+krystschr@users.noreply.github.com>
2022-02-13 10:01:05 +0100

    Revert "Aj order mangment menu methods"

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+// klasy:
+// pracownik => mechanik, księgowa;
+// samochód,
+// część,
+// narzędzia,
+// zlecenie
+
+
+namespace Warstat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        
+        
+        }
+    }
+}
