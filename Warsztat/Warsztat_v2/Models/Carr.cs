5086253457f74584b9b45c5603d51556f4ae00b6
Łukasz Kawecki <neoelektronika@gmail.com>
2022-03-02 15:42:35 +0100

    FunctionCarAdded

---
+++ Warsztat/Warsztat_v2/Models/Carr.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace Warsztat_v2.Models
+{
+    public class Carr
+
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Car Model")]
+        public string CarModel { get; set; }
+
+        [Display(Name = "Car Mark")]
+        public string CarMark { get; set; }
+
+        [Display(Name = "Production Year")]
+        public int YearProduction1 { get; set; }
+    }
+}
