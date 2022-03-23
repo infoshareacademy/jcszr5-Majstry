68746ffe7134947cdad9d8b0aadfff2610641e92
Artur Józwiak <artur_jozwiak@o2.pl>
2022-03-23 10:38:09 +0100

    Order Statistic viewComponent

---
+++ Warsztat/Warsztat_v2/Views/Order/Components/OrderStatisticViewModel.cs
@@ -0,0 +1,66 @@
+﻿using Warsztat.BLL.Services;
+using Warsztat.BLL.Enums;
+
+namespace Warsztat_v2.Views.Order.Components
+{
+    public class OrderStatisticViewModel
+    {
+        public int _waitingOrders { get; set; }
+        public int _inVerivicationOrders { get; set; }
+        public int _inProgressOrders { get; set; }
+        public int _finishedOrders { get; set; }
+        public int _cacncelledOrders { get; set; }
+
+        //IOrderService _orderService;
+        //public int _waitingOrders { get; set; }
+        //public int _inVerivicationOrders { get; set; }
+        //public int _inProgressOrders { get; set; }
+        //public int _finishedOrders { get; set; }
+        //public int _cacncelledOrders { get; set; }
+
+        //public OrderStatisticViewModel(IOrderService orderService)
+        //{
+        //    _orderService = orderService;
+
+        //    _waitingOrders = CalculateWaitingOrders();
+        //    _inVerivicationOrders = CalculateInVerivicationOrders();
+        //    _inProgressOrders = CalculateInProgresOrders();
+        //    _finishedOrders = CalculateFinishedOrders();
+        //    _cacncelledOrders = CalculateCanceledOrders();
+        //}
+
+
+
+        //public int CalculateWaitingOrders()
+        //{
+        //     _waitingOrders = _orderService.GetAll().Select(s => s.Status == Status.Waiting).Count();
+
+        //    return _waitingOrders;
+        //}
+        //public int CalculateInVerivicationOrders()
+        //{
+        //     _inVerivicationOrders = _orderService.GetAll().Select(s => s.Status == Status.Verification).Count();
+
+        //    return _inVerivicationOrders;
+        //}
+        //public int CalculateInProgresOrders()
+        //{
+        //    _inProgressOrders = _orderService.GetAll().Select(s => s.Status == Status.InProgress).Count();
+
+        //    return _inProgressOrders;
+        //}
+        //public int CalculateFinishedOrders()
+        //{
+        //     _finishedOrders = _orderService.GetAll().Select(s => s.Status == Status.Finished).Count();
+
+        //    return _finishedOrders;
+        //}
+        //public int CalculateCanceledOrders()
+        //{
+        //     _cacncelledOrders = _orderService.GetAll().Select(s => s.Status == Status.Cancelled).Count();
+
+        //    return _cacncelledOrders;
+        //}
+
+    }
+}
---
+++ Warsztat/Warsztat_v2/Views/Order/Components/OrderStatisticsViewComponent.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Warsztat.BLL.Services;
+using Warsztat.BLL.Models;
+using Warsztat.BLL.Enums;
+
+namespace Warsztat_v2.Views.Order.Components
+{
+    public class OrderStatisticsViewComponent : ViewComponent
+    {
+        private readonly IOrderService _orderService;
+
+        //private OrderStatisticViewModel _orderStatisticViewModel;
+        public OrderStatisticsViewComponent(IOrderService orderService /*OrderStatisticViewModel orderStatisticViewModel*/)
+        {
+            //_orderStatisticViewModel = new OrderStatisticViewModel(orderService);
+        
+            _orderService= orderService;    
+        }
+
+
+        public IViewComponentResult Invoke( )
+        {
+
+            var viewModel = new OrderStatisticViewModel/*(_orderService)*/
+            {
+                //_waitingOrders =_orderStatisticViewModel.CalculateWaitingOrders(),
+                //_inProgressOrders = _orderStatisticViewModel.CalculateInProgresOrders(),
+                //_inVerivicationOrders = _orderStatisticViewModel.CalculateInVerivicationOrders(),
+                //_finishedOrders = _orderStatisticViewModel.CalculateFinishedOrders(),
+                //_cacncelledOrders = _orderStatisticViewModel.CalculateCanceledOrders(),
+                _waitingOrders = _orderService.GetAll().Count(s=>s.Status == Status.Waiting),
+                _inProgressOrders = _orderService.GetAll().Count(s => s.Status == Status.InProgress),
+                _inVerivicationOrders = _orderService.GetAll().Count(s => s.Status == Status.Verification),
+                _finishedOrders = _orderService.GetAll().Count(s => s.Status == Status.Finished),
+                _cacncelledOrders = _orderService.GetAll().Count(s => s.Status == Status.Cancelled),
+
+            };
+            //viewModel._cacncelledOrders = viewModel.CalculateCanceledOrders();
+            return View(viewModel);
+        }
+    }
+}
